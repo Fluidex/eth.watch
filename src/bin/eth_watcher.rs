8053b0b6ff4baa12f51adbbd273f26bbab4da2b3
HAOYUatHZ <haoyu@protonmail.com>
2021-03-12 16:01:40 +0800

    update

--- src/bin/eth_watcher.rs
+++ src/bin/eth_watcher.rs
@@ -6,6 +6,15 @@ use tokio::{runtime::Runtime, time};
 fn main() {
     // Always print backtrace on panic.
     ::std::env::set_var("RUST_BACKTRACE", "1");
-    ::std::env::set_var("RUST_LOG", "Info");
+    match ::std::env::var("RUST_LOG") {
+        Ok(value) => {
+            if value.len() == 0 {
+                ::std::env::set_var("RUST_LOG", "info");
+            }
+        }
+        Err(_) => ::std::env::set_var("RUST_LOG", "info"),
+    }
     env_logger::init();
+
+    
 }
