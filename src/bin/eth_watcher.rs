4506be0f25dbd0af6b7c144641405b06a59e12f0
HAOYUatHZ <haoyu@protonmail.com>
2021-03-12 17:18:37 +0800

    go go go

--- src/bin/eth_watcher.rs
+++ src/bin/eth_watcher.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use futures::{channel::mpsc, SinkExt};
 use tokio::{runtime::Runtime, time};
 
-fn main() {
+fn init_logging() {
     // Always print backtrace on panic.
     ::std::env::set_var("RUST_BACKTRACE", "1");
     match ::std::env::var("RUST_LOG") {
@@ -14,7 +14,41 @@ fn main() {
         }
         Err(_) => ::std::env::set_var("RUST_LOG", "info"),
     }
-    env_logger::init();
+}
 
-    log::info!("hello world!");
+fn main() {
+    let mut main_runtime = Runtime::new().expect("main runtime start");
+
+    dotenv::dotenv().ok();
+    env_logger::init();
+    log::info!("ETH watcher started");
+
+    let mut conf = config_rs::Config::new();
+    let config_file = dotenv::var("CONFIG_FILE").unwrap();
+    conf.merge(config_rs::File::with_name(&config_file)).unwrap();
+    // let mut settings: config::Settings = conf.try_into().unwrap();
+    log::info!("{:?}", conf);
+
+
+    // let config = FluidexConfig::from_env();
+    // let client = EthereumGateway::from_config(&config);
+
+    // let (eth_req_sender, eth_req_receiver) = mpsc::channel(256);
+
+    // let eth_client = EthHttpClient::new(client, config.contracts.contract_addr);
+    // let watcher = EthWatch::new(eth_client, 0);
+
+    // main_runtime.spawn(watcher.run(eth_req_receiver));
+    // main_runtime.block_on(async move {
+    //     let mut timer = time::interval(Duration::from_secs(1));
+
+    //     loop {
+    //         timer.tick().await;
+    //         eth_req_sender
+    //             .clone()
+    //             .send(EthWatchRequest::PollETHNode)
+    //             .await
+    //             .expect("ETH watch receiver dropped");
+    //     }
+    // });
 }
