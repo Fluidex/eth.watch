ba1f66f655513588cef9ace67ad158f51cd526f6
HAOYUatHZ <haoyu@protonmail.com>
2021-03-12 15:01:25 +0800

    init log

--- src/bin/eth_watcher.rs
+++ src/bin/eth_watcher.rs
@@ -4,4 +4,8 @@ use futures::{channel::mpsc, SinkExt};
 use tokio::{runtime::Runtime, time};
 
 fn main() {
+    // Always print backtrace on panic.
+    ::std::env::set_var("RUST_BACKTRACE", "1");
+    ::std::env::set_var("RUST_LOG", "Info");
+    env_logger::init();
 }
