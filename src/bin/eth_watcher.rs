2624a2c2c68cee0c8caafa81b31e7c3ad0ff883a
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-03-12 16:21:23 +0800

    add logger (#1)
    * use bin
    
    * init main
    
    * init log
    
    * update
    
    * add logging

---
+++ src/bin/eth_watcher.rs
@@ -0,0 +1,20 @@
+use std::time::Duration;
+
+use futures::{channel::mpsc, SinkExt};
+use tokio::{runtime::Runtime, time};
+
+fn main() {
+    // Always print backtrace on panic.
+    ::std::env::set_var("RUST_BACKTRACE", "1");
+    match ::std::env::var("RUST_LOG") {
+        Ok(value) => {
+            if value.len() == 0 {
+                ::std::env::set_var("RUST_LOG", "info");
+            }
+        }
+        Err(_) => ::std::env::set_var("RUST_LOG", "info"),
+    }
+    env_logger::init();
+
+    log::info!("hello world!");
+}
