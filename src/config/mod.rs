26f0161cbe5b5447f45deb8e4f9c4e8adaac34f2
HAOYUatHZ <haoyu@protonmail.com>
2021-03-16 14:43:07 +0800

    update config.yaml

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -8,15 +8,6 @@ pub use crate::config::configs::{
 pub mod configs;
 
 #[derive(Debug, Deserialize, Clone, PartialEq)]
-#[serde(default)]
 pub struct Settings {
     pub contracts: ContractsConfig,
-}
-
-impl Default for Settings {
-    fn default() -> Self {
-        Settings {
-            contracts: ContractsConfig::default(),
-        }
-    }
 }
