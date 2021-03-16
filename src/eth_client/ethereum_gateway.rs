300ea6e3155d79021fb4d9755bab95e06654a401
HAOYUatHZ <haoyu@protonmail.com>
2021-03-16 12:47:57 +0800

    add web3

--- src/eth_client/ethereum_gateway.rs
+++ src/eth_client/ethereum_gateway.rs
@@ -1,6 +1,9 @@
+use std::fmt::Debug;
+
+use web3::contract::tokens::{Detokenize, Tokenize};
+use web3::contract::Options;
+use web3::types::{Address, BlockId, Filter, Log, U64};
+
 #[derive(Debug, Clone)]
 pub enum EthereumGateway {
-    // Direct(ETHDirectClient<PrivateKeySigner>),
-    // Multiplexed(MultiplexerEthereumClient),
-    // Mock(MockEthereum),
 }
