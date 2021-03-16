2ad397d0c89502f329e4be9aa4f18f1b166b02d2
HAOYUatHZ <haoyu@protonmail.com>
2021-03-16 20:48:29 +0800

    init for ETHState & WatcherMode

--- src/eth_watch/mod.rs
+++ src/eth_watch/mod.rs
@@ -17,35 +17,20 @@ pub enum EthWatchRequest {
     PollETHNode,
 }
 
-// pub struct EthWatch<W: EthClient> {
-//     client: W,
-//     eth_state: ETHState,
-//     /// All ethereum events are accepted after sufficient confirmations to eliminate risk of block reorg.
-//     number_of_confirmations_for_event: u64,
-//     mode: WatcherMode,
-// }
-
-// impl<W: EthClient> EthWatch<W> {
-//     pub fn new(client: W, number_of_confirmations_for_event: u64) -> Self {
-//         Self {
-//             client,
-//             eth_state: ETHState::default(),
-//             mode: WatcherMode::Working,
-//             number_of_confirmations_for_event,
-//         }
-//     }
-// }
-
 pub struct EthWatch<W: EthClient> {
     client: W,
+    eth_state: ETHState,
     /// All ethereum events are accepted after sufficient confirmations to eliminate risk of block reorg.
     number_of_confirmations_for_event: u64,
+    mode: WatcherMode,
 }
 
 impl<W: EthClient> EthWatch<W> {
     pub fn new(client: W, number_of_confirmations_for_event: u64) -> Self {
         Self {
             client,
+            eth_state: ETHState::default(),
+            mode: WatcherMode::Working,
             number_of_confirmations_for_event,
         }
     }
