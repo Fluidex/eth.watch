a279d98f736b0acb2b6f464e6f475e0ef6cd3a28
HAOYUatHZ <haoyu@protonmail.com>
2021-03-16 20:24:00 +0800

    init load contracts

---
+++ src/contracts/mod.rs
@@ -0,0 +1,25 @@
+use ethabi::Contract;
+use std::fs;
+use std::io;
+use std::str::FromStr;
+
+// TODO:
+const FLUIDEX_CONTRACT_FILE: &str = "fluidex.json";
+
+// TODO:
+fn read_file_to_json_value(path: &str) -> io::Result<serde_json::Value> {
+    let zksync_home = std::env::var("ZKSYNC_HOME").unwrap_or_else(|_| ".".into());
+    let path = std::path::Path::new(&zksync_home).join(path);
+    let contents = fs::read_to_string(path)?;
+    let val = serde_json::Value::from_str(&contents)?;
+    Ok(val)
+}
+
+pub fn fluidex_contract() -> Contract {
+    let abi_string = read_file_to_json_value(FLUIDEX_CONTRACT_FILE)
+        .expect("couldn't read FLUIDEX_CONTRACT_FILE")
+        .get("abi")
+        .expect("couldn't get abi from FLUIDEX_CONTRACT_FILE")
+        .to_string();
+    Contract::load(abi_string.as_bytes()).expect("fluidex contract abi")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod config;
+pub mod contracts;
 pub mod eth_client;
 pub mod eth_watch;
