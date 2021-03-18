3a1aa92af10b0c9d1c229018eea997c1e83247e8
HAOYUatHZ <haoyu@protonmail.com>
2021-03-18 17:23:56 +0800

    fix eth_state

--- src/eth_watch/eth_state.rs
+++ src/eth_watch/eth_state.rs
@@ -1,10 +1,6 @@
-// Built-in deps
 use std::collections::HashMap;
-// // External uses
-// // Workspace deps
-// use zksync_types::{PriorityOp, SerialId};
-// // Local deps
-// use super::received_ops::ReceivedPriorityOp;
+use crate::types::{PriorityOp, SerialId};
+use super::received_ops::ReceivedPriorityOp;
 
 /// Gathered state of the Ethereum network.
 /// Contains information about the known token types and incoming
@@ -18,19 +14,29 @@ use std::collections::HashMap;
 pub struct ETHState {
     /// The last block of the Ethereum network known to the Ethereum watcher.
     last_ethereum_block: u64,
-    // TODO: other fields
+    /// Queue of priority operations that are accepted by Ethereum network,
+    /// but not yet have enough confirmations to be processed by Fluidex.
+    ///
+    /// Note that since these operations do not have enough confirmations,
+    /// they may be not executed in the future, so this list is approximate.
+    ///
+    unconfirmed_queue: Vec<PriorityOp>,
+    /// Keys in this HashMap are numbers of blocks with `PriorityOp`.
+    /// Queue of priority operations that passed the confirmation
+    /// threshold and are waiting to be executed.
+    priority_queue: HashMap<u64, ReceivedPriorityOp>,
 }
 
 impl ETHState {
     pub fn new(
         last_ethereum_block: u64,
-        // unconfirmed_queue: Vec<PriorityOp>,
-        // priority_queue: HashMap<SerialId, ReceivedPriorityOp>,
+        unconfirmed_queue: Vec<PriorityOp>,
+        priority_queue: HashMap<SerialId, ReceivedPriorityOp>,
     ) -> Self {
         Self {
             last_ethereum_block,
-            // unconfirmed_queue,
-            // priority_queue,
+            unconfirmed_queue,
+            priority_queue,
         }
     }
 
@@ -38,11 +44,11 @@ impl ETHState {
         self.last_ethereum_block
     }
 
-    //     pub fn priority_queue(&self) -> &HashMap<u64, ReceivedPriorityOp> {
-    //         &self.priority_queue
-    //     }
+    pub fn priority_queue(&self) -> &HashMap<u64, ReceivedPriorityOp> {
+        &self.priority_queue
+    }
 
-    //     pub fn unconfirmed_queue(&self) -> &[PriorityOp] {
-    //         &self.unconfirmed_queue
-    //     }
+    pub fn unconfirmed_queue(&self) -> &[PriorityOp] {
+        &self.unconfirmed_queue
+    }
 }
