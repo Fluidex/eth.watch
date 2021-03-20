85c7710750d52ad18637d1f215518887d1e5e3de
HAOYUatHZ <haoyu@protonmail.com>
2021-03-20 19:30:42 +0800

    init other config files

--- src/config/configs/mod.rs
+++ src/config/configs/mod.rs
@@ -1,4 +1,6 @@
 // Public re-exports
-pub use self::contracts::ContractsConfig;
+pub use self::{contracts::ContractsConfig, eth_client::ETHClientConfig, eth_sender::ETHSenderConfig};
 
 pub mod contracts;
+pub mod eth_client;
+pub mod eth_sender;
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,12 +1,13 @@
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
-pub use crate::config::configs::ContractsConfig;
+pub use crate::config::configs::{ContractsConfig, ETHClientConfig, ETHSenderConfig};
 
 pub mod configs;
 
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 pub struct Settings {
-    // TODO:
     pub contracts: ContractsConfig,
+    pub eth_client: ETHClientConfig,
+    pub eth_sender: ETHSenderConfig,
 }
