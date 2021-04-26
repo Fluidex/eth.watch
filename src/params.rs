a95868a0befdbbd19125f3e4c0b1e2183cc22c1b
HAOYUatHZ <haoyu@protonmail.com>
2021-04-26 22:30:08 +0800

    add length params

--- src/params.rs
+++ src/params.rs
@@ -2,3 +2,14 @@ pub const ADDRESS_LEN: usize = 20;
 
 /// Priority op should be executed for this number of eth blocks.
 pub const PRIORITY_EXPIRATION: u64 = 35000;
+
+// TODO: check these
+pub const ACCOUNT_ID_BIT_WIDTH: usize = 32;
+pub const TOKEN_BIT_WIDTH: usize = 16;
+pub const TX_TYPE_BIT_WIDTH: usize = 8;
+pub const BALANCE_BIT_WIDTH: usize = 128;
+
+// Fr element encoding
+pub const FR_BIT_WIDTH: usize = 254;
+
+pub const FR_ADDRESS_LEN: usize = 20;
