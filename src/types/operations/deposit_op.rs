a76b31e948758af26ffb723b4e7abd8ce7681c01
HAOYUatHZ <haoyu@protonmail.com>
2021-04-26 22:18:46 +0800

    more on DepositOp

--- src/types/operations/deposit_op.rs
+++ src/types/operations/deposit_op.rs
@@ -1,1 +1,12 @@
+use crate::types::{AccountId, Deposit};
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DepositOp {
+    pub priority_op: Deposit,
+    pub account_id: AccountId,
+}
+
+impl DepositOp {
+    pub const CHUNKS: usize = 6; // TODO: ?
+    pub const OP_CODE: u8 = 0x01;
+}
