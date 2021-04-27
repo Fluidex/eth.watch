fcca0ec70b0819fa6f4fba84e3b8010532255679
HAOYUatHZ <haoyu@protonmail.com>
2021-04-27 17:04:58 +0800

    init NewTokenOp::try_from(event: Log)

--- src/types/new_token_op/mod.rs
+++ src/types/new_token_op/mod.rs
@@ -1,5 +1,8 @@
 use super::{utils::h256_as_vec, SerialId};
-use crate::basic_types::H256;
+use crate::basic_types::{H256, Log, U256};
+use anyhow::{bail, ensure, format_err};
+use serde::{Deserialize, Serialize};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct FluidexNewTokenOp {}
@@ -17,4 +20,33 @@ pub struct NewTokenOp {
     pub eth_hash: H256,
     /// Block in which Ethereum transaction was included.
     pub eth_block: u64,
+}
+
+
+impl TryFrom<Log> for NewTokenOp {
+    type Error = anyhow::Error;
+
+    fn try_from(event: Log) -> Result<NewTokenOp, anyhow::Error> {
+        let mut dec_ev = ethabi::decode(
+            &[
+                ethabi::ParamType::Address,
+                ethabi::ParamType::Uint(64),  // Serial id
+                ethabi::ParamType::Uint(8),   // OpType
+                ethabi::ParamType::Bytes,     // Pubdata
+                ethabi::ParamType::Uint(256), // expir. block
+            ],
+            &event.data.0,
+        )
+        .map_err(|e| format_err!("Event data decode: {:?}", e))?;
+
+        let sender = dec_ev.remove(0).to_address().unwrap();
+        Ok(NewTokenOp {
+            serial_id: dec_ev.remove(0).to_uint().as_ref().map(U256::as_u64).unwrap(),
+                // TODO:
+            data: FluidexNewTokenOp{},
+            deadline_block: dec_ev.remove(0).to_uint().as_ref().map(U256::as_u64).unwrap(),
+            eth_hash: event.transaction_hash.expect("Event transaction hash is missing"),
+            eth_block: event.block_number.expect("Event block number is missing").as_u64(),
+        })
+    }
 }
