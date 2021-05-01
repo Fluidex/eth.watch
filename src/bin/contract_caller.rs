43573e0eac53f6ce2e4a3c777af80ef00014994a
HAOYUatHZ <haoyu@protonmail.com>
2021-05-01 14:46:58 +0800

    change

--- src/bin/contract_caller.rs
+++ src/bin/contract_caller.rs
@@ -1,19 +1,25 @@
-extern crate web3;
+use web3::{
+    contract::{Contract, Options},
+    types::U256,
+};
 
-use web3::contract::{Contract, Options};
-use web3::types::Address;
-
-fn main() {
-    let http = web3::transports::Http::new("https://mainnet.infura.io").unwrap();
+#[tokio::main]
+async fn main() -> web3::contract::Result<()> {
+    let _ = env_logger::try_init();
+    let http = web3::transports::Http::new("http://localhost:8545")?;
     let web3 = web3::Web3::new(http);
 
-    // The contract address.
-    let address: Address = "0x...".parse().unwrap();
+    // Accessing existing contract
+    let contract_address = "0xd028d24f16a8893bd078259d413372ac01580769".parse().unwrap();
+    let contract = Contract::from_json(
+        web3.eth(),
+        contract_address,
+        include_bytes!("./abi.json"),
+    )?;
 
-    // Access the contract
-    let contract = Contract::from_json(web3.eth(), address, include_bytes!("./abi.json")).unwrap();
+    let result = contract.query("balanceOf", None, None, Options::default(), None);
+    let balance_of: U256 = result.await?;
+    assert_eq!(balance_of, 1_000_000.into());
 
-    // Query the contract instance
-    // And this is where I'm stuck!
-    let result = contract.query("decimals", None, None, web3::contract::Options::default(), None);
+    Ok(())
 }
