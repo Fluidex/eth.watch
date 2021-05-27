26848a5deb4aa1b1a1524de1076eda0a75b9d44f
HAOYUatHZ <haoyu@protonmail.com>
2021-05-27 17:22:34 +0800

    init RegUserOp::try_from(Log)

--- src/types/account/register_user.rs
+++ src/types/account/register_user.rs
@@ -17,3 +17,29 @@ pub struct RegUserOp {
     /// Block in which Ethereum transaction was included.
     pub eth_block: u64,
 }
+
+impl TryFrom<Log> for RegUserOp {
+    type Error = anyhow::Error;
+
+    fn try_from(event: Log) -> Result<RegUserOp, anyhow::Error> {
+        // let mut dec_ev = ethabi::decode(
+        //     &[
+        //         ethabi::ParamType::Address,  // token_address
+        //         ethabi::ParamType::Uint(16), // token_id
+        //     ],
+        //     &event.data.0,
+        // )
+        // .map_err(|e| format_err!("Event data decode: {:?}", e))?;
+
+        // let token_address = dec_ev.remove(0).to_address().unwrap();
+        // let token_id = dec_ev.remove(0).to_uint().as_ref().map(|ui| U256::as_u32(ui) as u16).unwrap();
+        Ok(RegUserOp {
+            data: FluidexRegUserOp {
+                // token_address,
+                // token_id: TokenId(token_id),
+            },
+            eth_hash: event.transaction_hash.expect("Event transaction hash is missing"),
+            eth_block: event.block_number.expect("Event block number is missing").as_u64(),
+        })
+    }
+}
