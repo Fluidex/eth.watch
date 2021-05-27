f887ed1d1d0647ac9be0e02ca7deb2f7ae89f1cf
HAOYUatHZ <haoyu@protonmail.com>
2021-05-27 16:40:05 +0800

    fix

--- scripts/fetch_gitsubmodule.sh
+++ scripts/fetch_gitsubmodule.sh
@@ -2,7 +2,7 @@
 set -uex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-REPO_DIR=$DIR/"../.."
+REPO_DIR=$DIR/".."
 cd $REPO_DIR
 
 git submodule update --init --recursive
