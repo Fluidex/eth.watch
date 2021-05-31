0f2879317be32fd5d024a950cb0020ef7f83d8b1
HAOYUatHZ <haoyu@protonmail.com>
2021-05-31 22:23:08 +0800

    minor

--- src/params.rs
+++ src/params.rs
@@ -4,7 +4,7 @@ pub const ADDRESS_LEN: usize = 20;
 pub const PRIORITY_EXPIRATION: u64 = 35000;
 
 // TODO: check these
-pub const ACCOUNT_ID_BIT_WIDTH: usize = 32;
+pub const ACCOUNT_ID_BIT_WIDTH: usize = 16;
 pub const TOKEN_BIT_WIDTH: usize = 16;
 pub const TX_TYPE_BIT_WIDTH: usize = 8;
 pub const BALANCE_BIT_WIDTH: usize = 256; // TODO: need to be consistent with .sol. zkSync use uint128.
